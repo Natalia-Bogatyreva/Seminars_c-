661a8c5ca31554686c3037506528a9ed5c408f99
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-17 22:07:45 +0500

    m n recurse

---
+++ recurse/Program.cs
@@ -0,0 +1,20 @@
+﻿// Рекурсия.
+// Задайте M и N. Вывести все натуральные чсила между ними.
+
+Console.WriteLine("Введите числа:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Numbers(m,n);
+
+void Numbers(int m, int n)
+{
+    if (m>n)
+    {
+        return;
+    }
+    Console.WriteLine(m);
+    m++;
+    Numbers(m,n);
+}
+
